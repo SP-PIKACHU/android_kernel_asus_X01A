99d32b65b09f2757440f30a030ccf9168980d0c3
kdrag0n <dragon@khronodragon.com>
2018-11-22 22:18:31 -0800

    msm-poweroff: disable download mode by default
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -63,9 +63,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-/* amt beg: disable download mode, modify this value from 1 to 0; */
-static int download_mode = 0;
-/* amt end: disable download mode, modify this value from 1 to 0; */
+static int download_mode;
 #else
 static const int download_mode;
 #endif
